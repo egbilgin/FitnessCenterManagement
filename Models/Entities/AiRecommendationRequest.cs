411261c01113aa2124d06790769ae28449a745c6
egbilgin <168185397+egbilgin@users.noreply.github.com>
2025-12-10 23:50:53 +0300

    crud işlemleri devam

--- Models/Entities/AiRecommendationRequest.cs
+++ Models/Entities/AiRecommendationRequest.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace FitnessCenterManagement.Models.Entities
-{
+{//namespace fitness
     public class AiRecommendationRequest
     {
         // Primary Key
