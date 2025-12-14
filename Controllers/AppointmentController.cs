61ee0ab36a304e75e924e4755649efeaa4409474
egbilgin <168185397+egbilgin@users.noreply.github.com>
2025-12-14 23:15:27 +0300

    controller

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-
+//14 aralık
 namespace FitnessCenterManagement.Controllers
 {
     [Authorize]
